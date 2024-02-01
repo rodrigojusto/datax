94600e7bf9ddd29b5c42e19596062100eef6129a
Rodrigo Souza Justo <rodrigo.justo@outlook.com>
2024-02-01 12:07:51 -0300

    ajusta o nome do provider para DataxPanelProvider.php.

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\Filament\AdminPanelProvider::class,
+        App\Providers\Filament\DataxPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
