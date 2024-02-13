6450b8b51bdb7218b5dfafe99c08819a3560d02d
Rodrigo Souza Justo <rodrigo.justo@outlook.com>
2024-02-13 20:36:11 -0300

    Altera a rota / para redirecionar para datax

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-/*
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/datax');
 });
 
+/*
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
